ba2327905cce54ec99f59aec8347566efe78b76a
Danne132 <163326801+Danner45@users.noreply.github.com>
2025-05-06 08:27:19 +0700

    Tạo Respos và Service của Supervisor

---
+++ GraduationProject/src/main/java/falcuty/ntu/groupone/graduation/services/ISupervisorService.java
@@ -0,0 +1,12 @@
+package falcuty.ntu.groupone.graduation.services;
+
+import java.util.List;
+
+import falcuty.ntu.groupone.graduation.models.Supervisor;
+
+public interface ISupervisorService {
+	public List<Supervisor> supervisors = null;
+	public List<Supervisor> getAllSupervisors();
+	public Supervisor saveSupervisor(Supervisor supervisor);
+	public Supervisor findSupervisorByName(String name);
+}
